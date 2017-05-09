1b0dd85dd4f4f08158d02c99f4fbbb28c75ada6b
Ville Kyrki <ville.kyrki@aalto.fi>
2017-05-09 15:42:53 +0300

    Added dump of all recordings and showing those.

---
+++ elisa-dump-program-info.py
@@ -0,0 +1,65 @@
+# Elisa Viihde API Python implementation usage demo
+
+import getopt, sys, getpass, elisaviihde, os, re
+import cPickle as pickle
+
+from subprocess import call
+
+def main():
+  # Parse command line args
+  if len(sys.argv[1:]) < 2:
+    print "ERROR: Usage:", sys.argv[0], "[-u username]" 
+    sys.exit(2)
+  try:
+    opts, args = getopt.getopt(sys.argv[1:], "u:v", ["username"])
+  except getopt.GetoptError as err:
+    print "ERROR:", str(err)
+    sys.exit(2) 
+  
+  # Init args
+  username = ""
+  verbose = False
+  
+  # Read arg data
+  for o, a in opts:
+    if o == "-v":
+      verbose = True
+    elif o in ("-u", "--username"):
+      username = a
+    else:
+      assert False, "unhandled option"
+  
+  # Ask password securely on command line
+  #password = getpass.getpass('Password: ')
+  password = "leevi3008"
+  
+  # Init elisa session
+  try:
+    elisa = elisaviihde.elisaviihde(verbose)
+  except Exception as exp:
+    print "ERROR: Could not create elisa session"
+    sys.exit(1)
+    
+  # Login
+  try:
+    elisa.login(username, password)
+  except Exception as exp:
+    print "ERROR: Login failed, check username and password"
+    sys.exit(1)
+    
+  allfolders = elisa.getallfolders()
+  
+  if verbose:
+    for folder in allfolders: print folder["name"]
+    
+  allrecordings = elisa.getallrecordings()
+  print str(len(allrecordings)) + " recordings total."
+
+  with open("recording_data.pkl", "wb") as output:
+    pickle.dump(allrecordings, output, pickle.HIGHEST_PROTOCOL)
+    
+  elisa.close()
+
+if __name__ == "__main__":
+  main()
+
---
+++ elisa-show-program-dump.py
@@ -0,0 +1,62 @@
+import re, sys
+import cPickle as pickle
+
+def main():
+  with open("recording_data.pkl", "rb") as input:
+    allrecordings = pickle.load(input)
+
+  totaltime = 0
+  for rec in allrecordings:
+    totaltime += rec["duration"]
+    try:
+      kausi = None
+      if "Kausi" in rec["description"] or "kausi" in rec["description"]:
+        m = re.search("[Kk]ausi ?([0-9]+)", rec["description"])
+        if m:
+          kausi = m.group(1)
+        else:
+          m = re.search("([0-9]+)\. ?[kK]ausi", rec["description"])
+          if m:
+            kausi = m.group(1)
+          else:
+            m = re.search("([0-9]+)\. ?[tT]uotantokausi", rec["description"])
+            if m:
+              kausi = m.group(1)
+
+#      if "kauden" in rec["description"]: print rec["description"]
+#      if "Kauden" in rec["description"]: print rec["description"]
+              
+      jakso = None
+      if "/" in rec["description"]:
+        m = re.search("([0-9]+) ?/([0-9]+)", rec["description"])
+        if m:
+          if m.group(2)>2:
+            jakso = m.group(1)
+#          else:
+#            print "J " + jakso + " / " + m.group(2)
+      if "osa" in rec["description"]:
+        m = re.search("osa ([0-9]+)", rec["description"])
+        if m:
+            jakso = m.group(1)
+
+#      if kausi and not jakso: print rec["description"]
+      pv = None
+      m = re.search("(\d+)\.(\d+)\.(\d+)", rec["startTime"])
+      if m:
+          pv = m.group(1)
+          kk = m.group(2)
+          vvvv = m.group(3)
+
+      filename = rec["name"]
+      if kausi and jakso: filename += "_K" + kausi + "J" + jakso.zfill(2)
+      if not kausi and jakso: filename += "_J" + jakso.zfill(2) 
+      if pv: filename += "_" + vvvv + kk + pv
+      
+      print filename
+    except KeyError:
+      continue
+    
+  print "Total duration " + str(totaltime) + " sec (" + str(totaltime/60) + " min = " + str(totaltime/3600) + " hr)"
+
+if __name__ == "__main__":
+  main()
